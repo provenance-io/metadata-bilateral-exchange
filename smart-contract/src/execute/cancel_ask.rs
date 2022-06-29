75d7296d172448ecd893ab555beb0719121fa4d5
hyperschwartz <jschwartz@figure.com>
2022-06-29 15:01:43 -0700

    Finish cancel_ask tests

--- smart-contract/src/execute/cancel_ask.rs
+++ smart-contract/src/execute/cancel_ask.rs
@@ -78,18 +78,147 @@ mod tests {
     use crate::execute::create_ask::create_ask;
     use crate::storage::ask_order_storage::insert_ask_order;
     use crate::test::cosmos_type_helpers::single_attribute_for_key;
-    use crate::test::mock_instantiate::default_instantiate;
-    use crate::test::mock_marker::{MockMarker, DEFAULT_MARKER_DENOM};
+    use crate::test::mock_instantiate::{default_instantiate, DEFAULT_ADMIN_ADDRESS};
+    use crate::test::mock_marker::{MockMarker, DEFAULT_MARKER_DENOM, DEFAULT_MARKER_HOLDINGS};
+    use crate::test::mock_scope::{MockScope, DEFAULT_SCOPE_ID};
+    use crate::test::request_helpers::mock_ask_order;
     use crate::types::core::msg::ExecuteMsg;
     use crate::types::request::ask_types::ask::Ask;
     use crate::types::request::ask_types::ask_order::AskOrder;
+    use crate::types::request::share_sale_type::ShareSaleType;
     use cosmwasm_std::testing::{mock_env, mock_info, MOCK_CONTRACT_ADDR};
-    use cosmwasm_std::{coins, from_binary, Addr, CosmosMsg};
+    use cosmwasm_std::{coins, from_binary, CosmosMsg, Storage};
     use provwasm_mocks::mock_dependencies;
-    use provwasm_std::{MarkerMsgParams, ProvenanceMsgParams};
+    use provwasm_std::{MarkerMsgParams, MetadataMsgParams, PartyType, ProvenanceMsgParams};
 
     #[test]
-    fn cancel_coin_ask_with_valid_data() {
+    fn cancel_coin_ask_as_asker() {
+        do_coin_trade_cancel_ask("asker");
+    }
+
+    #[test]
+    fn test_cancel_ask_as_admin() {
+        do_coin_trade_cancel_ask(DEFAULT_ADMIN_ADDRESS);
+    }
+
+    #[test]
+    fn test_cancel_marker_trade_ask_as_asker() {
+        do_marker_trade_cancel_ask("asker");
+    }
+
+    #[test]
+    fn test_cancel_marker_trade_ask_as_admin() {
+        do_marker_trade_cancel_ask(DEFAULT_ADMIN_ADDRESS);
+    }
+
+    #[test]
+    fn test_cancel_marker_share_sale_ask_as_asker() {
+        do_marker_share_sale_cancel_ask("asker");
+    }
+
+    #[test]
+    fn test_cancel_marker_share_sale_ask_as_admin() {
+        do_marker_share_sale_cancel_ask(DEFAULT_ADMIN_ADDRESS);
+    }
+
+    #[test]
+    fn test_cancel_scope_trade_ask_as_asker() {
+        do_scope_trade_cancel_test("asker");
+    }
+
+    #[test]
+    fn test_cancel_scope_trade_ask_as_admin() {
+        do_scope_trade_cancel_test(DEFAULT_ADMIN_ADDRESS);
+    }
+
+    #[test]
+    fn test_cancel_ask_with_invalid_data() {
+        let mut deps = mock_dependencies(&[]);
+        default_instantiate(deps.as_mut().storage);
+        let err = cancel_ask(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("asker", &[]),
+            String::new(),
+        )
+        .expect_err("an error should occur when no id is provided");
+        match err {
+            ContractError::ValidationError { messages } => {
+                assert_eq!(
+                    1,
+                    messages.len(),
+                    "only one validation error message should be produced",
+                );
+                assert_eq!(
+                    "an id must be provided when cancelling an ask",
+                    messages.first().unwrap(),
+                    "the correct validation error should be produced",
+                );
+            }
+            e => panic!("unexpected error: {:?}", e),
+        };
+        let err = cancel_ask(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("asker", &coins(100, "nhash")),
+            "ask_id".to_string(),
+        )
+        .expect_err("an error should occur when funds are provided");
+        assert!(
+            matches!(err, ContractError::InvalidFundsProvided { .. }),
+            "an invalid funds error should be produced when funds are provided, but got: {:?}",
+            err,
+        );
+        let err = cancel_ask(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("asker", &[]),
+            "ask_id".to_string(),
+        )
+        .expect_err("an error should occur when no ask can be found by the provided id");
+        assert!(
+            matches!(err, ContractError::StorageError { .. }),
+            "a storage error should be produced when no ask can be found by the provided id, but got: {:?}",
+            err,
+        );
+        let ask_order = mock_ask_order(AskCollateral::coin_trade(&[], &[]));
+        insert_ask_order(deps.as_mut().storage, &ask_order)
+            .expect("inserting an ask order should succeed");
+        assert_eq!(
+            "ask_id", ask_order.id,
+            "sanity check: the mock ask order should have the expected default id",
+        );
+        let err = cancel_ask(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("impostor", &[]),
+            "ask_id".to_string(),
+        ).expect_err("an error should be produced when an account other than the asker or admin tries to cancel an ask");
+        assert!(
+            matches!(err, ContractError::Unauthorized),
+            "an unauthorized error should be produced when an invalid account tries to cancel an ask, but got: {:?}",
+            err,
+        );
+    }
+
+    fn assert_cancel_ask_succeeded(storage: &dyn Storage, response: &Response<ProvenanceMsg>) {
+        assert_eq!(
+            2,
+            response.attributes.len(),
+            "the response should have the correct number of attributes",
+        );
+        assert_eq!("cancel_ask", single_attribute_for_key(&response, "action"),);
+        assert_eq!("ask_id", single_attribute_for_key(&response, "ask_id"),);
+        let response_data_ask_order = if let Some(ref binary) = response.data {
+            from_binary::<AskOrder>(binary).expect("response data deserialize correctly")
+        } else {
+            panic!("response data should be set");
+        };
+        get_ask_order_by_id(storage, &response_data_ask_order.id)
+            .expect_err("the ask should no longer be available in storage after a cancellation");
+    }
+
+    fn do_coin_trade_cancel_ask<S: Into<String>>(sender_address: S) {
         let mut deps = mock_dependencies(&[]);
         default_instantiate(&mut deps.storage);
 
@@ -110,150 +239,41 @@ mod tests {
         assert!(get_ask_order_by_id(deps.as_ref().storage, "ask_id").is_ok());
 
         // cancel ask order
-        let asker_info = mock_info("asker", &[]);
+        let asker_info = mock_info(&sender_address.into(), &[]);
 
         let cancel_ask_msg = ExecuteMsg::CancelAsk {
             id: "ask_id".to_string(),
         };
-        let cancel_ask_response = execute(
+        let response = execute(
             deps.as_mut(),
             mock_env(),
             asker_info.clone(),
             cancel_ask_msg,
+        )
+        .expect("expected the coin trade ask to be cancelled successfully");
+        assert_cancel_ask_succeeded(deps.as_ref().storage, &response);
+        assert_eq!(
+            1,
+            response.messages.len(),
+            "expected only a single message to be sent when cancelling a coin trade",
         );
-
-        match cancel_ask_response {
-            Ok(cancel_ask_response) => {
-                assert_eq!(cancel_ask_response.attributes.len(), 2);
-
+        match &response.messages.first().unwrap().msg {
+            CosmosMsg::Bank(BankMsg::Send { to_address, amount }) => {
                 assert_eq!(
-                    "cancel_ask",
-                    single_attribute_for_key(&cancel_ask_response, "action")
+                    "asker", to_address,
+                    "the asker address should be the target of the send message",
                 );
                 assert_eq!(
-                    "ask_id",
-                    single_attribute_for_key(&cancel_ask_response, "ask_id"),
-                );
-                assert_eq!(cancel_ask_response.messages.len(), 1);
-                assert_eq!(
-                    cancel_ask_response.messages[0].msg,
-                    CosmosMsg::Bank(BankMsg::Send {
-                        to_address: asker_info.sender.to_string(),
-                        amount: coins(200, "base_1"),
-                    })
+                    &coins(200, "base_1"),
+                    amount,
+                    "the sent amount should be correct",
                 );
             }
-            Err(error) => panic!("unexpected error: {:?}", error),
-        }
-
-        // verify ask order removed from storage
-        assert!(get_ask_order_by_id(deps.as_ref().storage, "ask_id").is_err());
-    }
-
-    #[test]
-    fn cancel_coin_ask_with_invalid_data() {
-        let mut deps = mock_dependencies(&[]);
-        default_instantiate(&mut deps.storage);
-
-        let asker_info = mock_info("asker", &[]);
-
-        // cancel ask order with missing id returns ContractError::Unauthorized
-        let cancel_ask_msg = ExecuteMsg::CancelAsk { id: "".to_string() };
-        let cancel_response = execute(
-            deps.as_mut(),
-            mock_env(),
-            asker_info.clone(),
-            cancel_ask_msg,
-        )
-        .expect_err("expected an error to occur when a blank id is provided");
-
-        match cancel_response {
-            ContractError::ValidationError { messages } => {
-                assert_eq!(1, messages.len());
-                assert_eq!(
-                    "an id must be provided when cancelling an ask",
-                    messages.first().unwrap(),
-                );
-            }
-            e => panic!("unexpected error with invalid id provided: {:?}", e),
-        }
-
-        // cancel non-existent ask order returns ContractError::Unauthorized
-        let cancel_ask_msg = ExecuteMsg::CancelAsk {
-            id: "unknown_id".to_string(),
-        };
-
-        let cancel_response = execute(
-            deps.as_mut(),
-            mock_env(),
-            asker_info.clone(),
-            cancel_ask_msg,
-        )
-        .expect_err("expected an error to occur for a missing ask");
-
-        match cancel_response {
-            ContractError::StorageError { message } => {
-                assert_eq!(
-                    "failed to find AskOrder by id [unknown_id]: NotFound { kind: \"metadata_bilateral_exchange::types::request::ask_types::ask_order::AskOrder\" }",
-                    message,
-                );
-            }
-            e => panic!("unexpected error when cancelling missing ask: {:?}", e),
-        }
-
-        // cancel ask order with sender not equal to owner returns ContractError::Unauthorized
-        if let Err(error) = insert_ask_order(
-            &mut deps.storage,
-            &AskOrder::new_unchecked(
-                "ask_id",
-                Addr::unchecked(""),
-                AskCollateral::coin_trade(&coins(200, "base_1"), &coins(100, "quote_1")),
-                None,
-            ),
-        ) {
-            panic!("unexpected error: {:?}", error)
-        };
-        let cancel_ask_msg = ExecuteMsg::CancelAsk {
-            id: "ask_id".to_string(),
-        };
-
-        let cancel_response = execute(deps.as_mut(), mock_env(), asker_info, cancel_ask_msg);
-
-        match cancel_response {
-            Err(error) => match error {
-                ContractError::Unauthorized => {}
-                _ => {
-                    panic!("unexpected error: {:?}", error)
-                }
-            },
-            Ok(_) => panic!("expected error, but cancel_response ok"),
-        }
-
-        // cancel ask order with sent_funds returns ContractError::CancelWithFunds
-        let asker_info = mock_info("asker", &coins(1, "sent_coin"));
-        let cancel_ask_msg = ExecuteMsg::CancelAsk {
-            id: "ask_id".to_string(),
-        };
-
-        let cancel_response = execute(deps.as_mut(), mock_env(), asker_info, cancel_ask_msg)
-            .expect_err("expected an error to occur when sending funds");
-
-        match cancel_response {
-            ContractError::InvalidFundsProvided { message } => {
-                assert_eq!(
-                    "funds should not be provided when cancelling an ask",
-                    message,
-                );
-            }
-            e => panic!(
-                "unexpected error occurred when sending funds while cancelling an ask: {:?}",
-                e
-            ),
+            msg => panic!("unexpected message in response: {:?}", msg),
         }
     }
 
-    #[test]
-    fn test_cancel_marker_trade_ask_with_valid_data() {
+    fn do_marker_trade_cancel_ask<S: Into<String>>(sender_address: S) {
         let mut deps = mock_dependencies(&[]);
         default_instantiate(&mut deps.storage);
         let ask_id = "ask_id".to_string();
@@ -267,15 +287,52 @@ mod tests {
             None,
         )
         .expect("marker trade ask should be created without issue");
-        let ask_order = get_ask_order_by_id(&mut deps.storage, &ask_id)
+        get_ask_order_by_id(&mut deps.storage, &ask_id)
             .expect("an ask order should be available in storage");
         let response = cancel_ask(
             deps.as_mut(),
             mock_env(),
-            mock_info("asker", &[]),
-            ask_id.to_owned(),
+            mock_info(&sender_address.into(), &[]),
+            ask_id,
         )
         .expect("cancel ask should succeed");
+        assert_cancel_ask_succeeded(deps.as_ref().storage, &response);
+        assert_marker_response_sent_proper_messages(&response);
+    }
+
+    fn do_marker_share_sale_cancel_ask<S: Into<String>>(sender_address: S) {
+        let mut deps = mock_dependencies(&[]);
+        default_instantiate(deps.as_mut().storage);
+        let ask_id = "ask_id".to_string();
+        deps.querier
+            .with_markers(vec![MockMarker::new_owned_marker("asker")]);
+        create_ask(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("asker", &[]),
+            Ask::new_marker_share_sale(
+                &ask_id,
+                DEFAULT_MARKER_DENOM,
+                &coins(150, "nhash"),
+                ShareSaleType::single(DEFAULT_MARKER_HOLDINGS),
+            ),
+            None,
+        )
+        .expect("expected the marker share sale to be created successfully");
+        get_ask_order_by_id(deps.as_ref().storage, &ask_id)
+            .expect("an ask order should be available in storage");
+        let response = cancel_ask(
+            deps.as_mut(),
+            mock_env(),
+            mock_info(&sender_address.into(), &[]),
+            ask_id,
+        )
+        .expect("expected cancel ask to succeed");
+        assert_cancel_ask_succeeded(deps.as_ref().storage, &response);
+        assert_marker_response_sent_proper_messages(&response);
+    }
+
+    fn assert_marker_response_sent_proper_messages(response: &Response<ProvenanceMsg>) {
         assert_eq!(
             2,
             response.messages.len(),
@@ -321,25 +378,76 @@ mod tests {
             },
             msg => panic!("unexpected message produced when cancelling a marker ask: {:?}", msg),
         });
-        assert_eq!(
-            2,
-            response.attributes.len(),
-            "the response should have the correct number of attributes",
-        );
-        assert_eq!("cancel_ask", single_attribute_for_key(&response, "action"),);
-        assert_eq!("ask_id", single_attribute_for_key(&response, "ask_id"),);
-        let response_data_ask_order = from_binary::<AskOrder>(
-            &response.data.expect("response data should be set"),
-        )
-        .expect("the response data should be able to be converted from binary to an AskOrder");
-        assert_eq!(
-            ask_order, response_data_ask_order,
-            "the response data's AskOrder should equate to the cancelled ask order",
-        );
-        get_ask_order_by_id(&deps.storage, &ask_id)
-            .expect_err("the ask should no longer be available in storage after a cancellation");
     }
 
-    #[test]
-    fn test_cancel_marker_trade_ask_with_invalid_data() {}
+    fn do_scope_trade_cancel_test<S: Into<String>>(sender_address: S) {
+        let mut deps = mock_dependencies(&[]);
+        default_instantiate(deps.as_mut().storage);
+        let ask_id = "ask_id".to_string();
+        deps.querier
+            .with_scope(MockScope::new_with_owner(MOCK_CONTRACT_ADDR));
+        create_ask(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("asker", &[]),
+            Ask::new_scope_trade(&ask_id, DEFAULT_SCOPE_ID, &coins(100, "nhash")),
+            None,
+        )
+        .expect("expected the scope trade to be created successfully");
+        get_ask_order_by_id(deps.as_ref().storage, &ask_id)
+            .expect("an ask order should be available in storage");
+        let response = cancel_ask(
+            deps.as_mut(),
+            mock_env(),
+            mock_info(&sender_address.into(), &[]),
+            ask_id,
+        )
+        .expect("expected cancel ask to succeed");
+        assert_cancel_ask_succeeded(deps.as_ref().storage, &response);
+        assert_eq!(
+            1,
+            response.messages.len(),
+            "a scope trade cancellation should send the correct number of messages",
+        );
+        match &response.messages.first().unwrap().msg {
+            CosmosMsg::Custom(ProvenanceMsg {
+                params:
+                    ProvenanceMsgParams::Metadata(MetadataMsgParams::WriteScope { scope, signers }),
+                ..
+            }) => {
+                assert_eq!(
+                    "asker",
+                    scope.value_owner_address.as_str(),
+                    "the asker should be assigned as the scope value owner",
+                );
+                assert_eq!(
+                    1,
+                    scope.owners.len(),
+                    "there should only be a single scope owner",
+                );
+                let owner = scope.owners.first().unwrap();
+                assert_eq!(
+                    "asker",
+                    owner.address.as_str(),
+                    "the owner address should be the asker",
+                );
+                assert_eq!(
+                    PartyType::Owner,
+                    owner.role,
+                    "the role of the owner should be PartyType::Owner",
+                );
+                assert_eq!(
+                    1,
+                    signers.len(),
+                    "there should be a single signer for the scope write",
+                );
+                assert_eq!(
+                    MOCK_CONTRACT_ADDR,
+                    signers.first().unwrap().as_str(),
+                    "the contract's address should be the signer on the scope write",
+                );
+            }
+            msg => panic!("unexpected message when cancelling scope trade: {:?}", msg),
+        };
+    }
 }
